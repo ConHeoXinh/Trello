d43ac44af5fe3f5a604597408c1daf3a669fd7b4
= <=>
2023-11-18 09:27:42 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-/* eslint-disable react/jsx-no-target-blank */
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import {
-  Experimental_CssVarsProvider as CssVarsProvider,
-  experimental_extendTheme as extendTheme,
-  useColorScheme,
-} from "@mui/material/styles";
-
 function App() {
-  return (
-    <>
-      <hr />
-      <Typography variant="body2" component="h2" color="text.secondary">
-        dsfsdf
-      </Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
