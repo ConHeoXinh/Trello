3ffa563694bb091e620a320b092cd74edce4229b
= <=>
2023-11-20 16:26:43 +0700

    drag drop columns

--- src/pages/Boards/BoardContent/ListColumns/Colum/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Colum/ListCards/ListCards.jsx
@@ -1,29 +1,30 @@
 import { Box } from '@mui/material'
 import TrelloCard from './Card/TrelloCard'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListCards({ cards }) {
   return (
-    <Box sx={{
-      display:'flex',
-      flexDirection:'column',
-      p: '0 5px',
-      m: '0 5px',
-      gap:1,
-      overflowX:'hidden',
-      overflowY:'auto',
-      maxHeight: (theme) => `calc(
+    <SortableContext items={cards?.map(c => c._id)} strategy={verticalListSortingStrategy}>
+      <Box sx={{
+        display:'flex',
+        flexDirection:'column',
+        p: '0 5px',
+        m: '0 5px',
+        gap:1,
+        overflowX:'hidden',
+        overflowY:'auto',
+        maxHeight: (theme) => `calc(
     ${theme.trello.boardContentHeight} - 
     ${theme.spacing(5)} -
     ${theme.trello.columnHeaderHeight} -
     ${theme.trello.columnFooterHeight})`,
-      '&::-webkit-scrollbar-track': { backgroundColor: '#ced0da' },
-      '&::-webkit-scrollbar-thumb': { backgroundColor: '#bfccf' }
-    }}>
+        '&::-webkit-scrollbar-track': { backgroundColor: '#ced0da' },
+        '&::-webkit-scrollbar-thumb': { backgroundColor: '#bfccf' }
+      }}>
 
-      {cards?.map(card => <TrelloCard key={card._id} card={card}/>)}
-      {/* <TrelloCard/>
-      <TrelloCard temporaryHideMedia/> */}
-    </Box>
+        {cards?.map(card => <TrelloCard key={card._id} card={card}/>)}
+      </Box>
+    </SortableContext>
   )
 }
 
